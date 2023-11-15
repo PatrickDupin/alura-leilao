bbef47b54a152ae6c88671ca8f37bff2a59971a9
PatrickDupin <patrick.dupin@feedz.com.br>
2023-11-15 11:32:26 -0300

    implementação de testes de unidade

---
+++ src/Model/Leilao.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Alura\Leilao\Model;
+
+class Leilao
+{
+    /** @var Lance[] */
+    private $lances;
+
+    /** @var string */
+    private $descricao;
+
+    /** @var bool */
+    private $finalizado;
+
+    public function __construct(string $descricao)
+    {
+        $this->descricao = $descricao;
+        $this->lances = [];
+        $this->finalizado = false;
+    }
+
+    private function ehDoUltimoUsuario($lance): bool
+    {
+        $ultimoLance = $this->lances[count($this->lances) - 1];
+        return $lance->getUsuario() == $ultimoLance->getUsuario();
+    }
+
+    private function quantidadeLancesPorUsuario(Usuario $usuario): int
+    {
+        return array_reduce(
+            $this->lances,
+            function (int $totalAcumulado, Lance $lanceAtual) use ($usuario) {
+                if ($lanceAtual->getUsuario() == $usuario) {
+                    return $totalAcumulado + 1;
+                }
+                return $totalAcumulado;
+            },
+            0
+        );
+    }
+
+    public function recebeLance(Lance $lance)
+    {
+        if (!empty($this->lances) && $this->ehDoUltimoUsuario($lance)) {
+            throw new \DomainException("Usuário não pode propor 2 lances seguidos");
+        }
+
+        $totalLancesUsuario = $this->quantidadeLancesPorUsuario($lance->getUsuario());
+        if ($totalLancesUsuario >= 5) {
+            throw new \DomainException("Usuário não propor mais de 5 lances por leilão");
+        }
+
+        $this->lances[] = $lance;
+    }
+
+    /**
+     * @return Lance[]
+     */
+    public function getLances(): array
+    {
+        return $this->lances;
+    }
+
+    public function finaliza()
+    {
+        $this->finalizado = true;
+    }
+
+    public function estaFinalizado()
+    {
+        return $this->finalizado;
+    }
+}
